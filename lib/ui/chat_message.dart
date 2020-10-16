761ce3e05be1e5003dfcd3edc017e10e691dddef
Sergiomanoel <44680691+Sergiomanoel@users.noreply.github.com>
2020-10-16 15:11:07 -0300

    Update chat_message.dart

--- lib/ui/chat_message.dart
+++ lib/ui/chat_message.dart
@@ -1,1 +1,20 @@
+import 'package:flutter/material.dart';
 
+class ChatMessage extends StatelessWidget {
+  ChatMessage(this.data, this.mine);
+  final Map<String, dynamic> data;
+  final bool mine;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(10.0),
+      child: Row(
+        children: [
+          !mine
+              ? Padding(
+                  padding: EdgeInsets.only(right: 16.0),
+                  child: CircleAvatar(
+                    backgroundImage: NetworkImage(data["SenderPhotoUrl"]),
+                  ),
+                )
