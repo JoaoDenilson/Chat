f4bb167774ae6cba931df088283ed70bbd191d7a
ramon0306 <ramonbatista0306@gmail.com>
2020-10-16 15:00:00 -0300

    Criando a pasta chat_message

--- lib/helpers/firebase_helper.dart
+++ lib/helpers/firebase_helper.dart
@@ -1,5 +1,3 @@
-
-
 import 'dart:io';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -13,61 +11,66 @@ class FirebaseHelper {
 
   factory FirebaseHelper() => _instance;
 
-  FirebaseHelper.internal(){
+  FirebaseHelper.internal() {
     //Escutar qual quer alteração do cliente
-    FirebaseAuth.instance.authStateChanges().listen((user) { 
+    FirebaseAuth.instance.authStateChanges().listen((user) {
       this.currentUser = user;
     });
   }
 
-  
+  Stream<QuerySnapshot> snapshots() {
+    return FirebaseFirestore.instance
+        .collection("messages")
+        .orderBy("time")
+        .snapshots();
+  }
 
   Future<User> getUser() async {
-
-    if(currentUser != null) return currentUser;
-    //Login com o google  
+    if (currentUser != null) return currentUser;
+    //Login com o google
     final GoogleSignIn googleSingin = GoogleSignIn();
     final GoogleSignInAccount googleSignInAccount = await googleSingin.signIn();
 
     //Login no firabase
-    final GoogleSignInAuthentication googleSignInAuthentication = await googleSignInAccount.authentication;
-    //Credenciais  
+    final GoogleSignInAuthentication googleSignInAuthentication =
+        await googleSignInAccount.authentication;
+    //Credenciais
     final OAuthCredential credential = GoogleAuthProvider.credential(
-      accessToken: googleSignInAuthentication.accessToken,
-      idToken: googleSignInAuthentication.idToken
-    );
+        accessToken: googleSignInAuthentication.accessToken,
+        idToken: googleSignInAuthentication.idToken);
     //Fazer login no Firebase
-    UserCredential userCredential = await FirebaseAuth.instance.signInWithCredential(credential);
+    UserCredential userCredential =
+        await FirebaseAuth.instance.signInWithCredential(credential);
     return userCredential.user;
-
-  } 
+  }
 
   Future<void> sendMessage(String text) async {
-    User user =  await getUser();
+    User user = await getUser();
 
     FirebaseFirestore.instance.collection("messages").add({
       "uid": user.uid,
       "senderName": user.displayName,
-      "senderPhotoUrl": user.photoURL, 
+      "senderPhotoUrl": user.photoURL,
       "text": text,
       "time": Timestamp.now()
     });
   }
 
   Future<void> sendImage(File file) async {
-    User user =  await getUser();
+    User user = await getUser();
 
-    StorageUploadTask task = FirebaseStorage.instance.ref().child('imgs').child(
-      DateTime.now().millisecondsSinceEpoch.toString()
-    ).putFile(file);
+    StorageUploadTask task = FirebaseStorage.instance
+        .ref()
+        .child('imgs')
+        .child(DateTime.now().millisecondsSinceEpoch.toString())
+        .putFile(file);
     StorageTaskSnapshot taskSnapshot = await task.onComplete;
     String url = await taskSnapshot.ref.getDownloadURL();
-
 
     FirebaseFirestore.instance.collection("messages").add({
       "uid": user.uid,
       "senderName": user.displayName,
-      "senderPhotoUrl": user.photoURL, 
+      "senderPhotoUrl": user.photoURL,
       "imgUrl": url,
       "time": Timestamp.now()
     });
--- lib/ui/chat_page.dart
+++ lib/ui/chat_page.dart
@@ -1,5 +1,8 @@
+import 'package:chatpdm/helpers/firebase_helper.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:chatpdm/ui/text_composer.dart';
+import 'package:chatpdm/ui/chat_message.dart';
 
 class ChatPage extends StatefulWidget {
   @override
@@ -7,6 +10,8 @@ class ChatPage extends StatefulWidget {
 }
 
 class _ChatPageState extends State<ChatPage> {
+  FirebaseHelper helper = FirebaseHelper();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,7 +20,35 @@ class _ChatPageState extends State<ChatPage> {
         centerTitle: true,
       ),
       body: Column(
-        children: [Container(), TextComposer()],
+        children: [
+          Expanded(
+            child: StreamBuilder(
+              stream: helper.snapshots(),
+              builder: (context, snapshot) {
+                switch (snapshot.connectionState) {
+                  case ConnectionState.none:
+                  case ConnectionState.waiting:
+                    return Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  default:
+                    List<DocumentSnapshot> documents =
+                        snapshot.data.documents.reversed.toList();
+                    return ListView.builder(
+                        itemCount: documents.length,
+                        reverse: true,
+                        itemBuilder: (contex, index) {
+                          return ChatMessage(
+                              documents[index].data(),
+                              documents[index].data()["uid"] ==
+                                  helper.currentUser?.uid);
+                        });
+                }
+              },
+            ),
+          ),
+          TextComposer()
+        ],
       ),
     );
   }
